8687b5b275080fc9f56038a6eb68cde7c765a421
SupeChicken666 <me@supechicken666.dev>
2026-01-27 00:11:24 +0800

    Bring back SetupWizard
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- product.mk
+++ product.mk
@@ -32,9 +32,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/waydroid.prop:$(TARGET_COPY_OUT_VENDOR)/waydroid.prop
 
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.setupwizard.mode=DISABLED
-
 # PC mode
 PRODUCT_PACKAGES += \
     pc.xml
